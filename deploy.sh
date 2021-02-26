a3d2292034dfabae24eb4d9e77b2bb2b5d5f5e8b
acs1899 <yrn19901005@gmail.com>
2021-02-26 17:54:34 +0800

    更新发布脚本

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,8 @@ version=`jq '.version' package.json | sed 's/\"//g'`
 
 yarn build
 
-zip -r ngaImgBox_v$version.zip dist/
+cd dist
+
+zip -r ngaImgBox_v$version.zip ./*
+
+mv ngaImgBox_v$version.zip ../
